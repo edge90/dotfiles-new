655c7dd4eb8ec0c98839765671aef3c51183b288
Niclas Larsson <niclas@edgesystems.se>
2021-10-27 14:29:37 +0200

    Git shortcuts in fish.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,8 +14,8 @@ set --export PATH $HOME/.local/bin $PATH
 set --export SSH_AUTH_SOCK $HOME/.ssh/agent.sock
 
 # Enable waylad (move to .pamenvironment?)
-set --export MOZ_ENABLE_WAYLAND 1
-set --export GDK_BACKEND wayland
+#set --export MOZ_ENABLE_WAYLAND 1
+#set --export GDK_BACKEND wayland
 
 set --export EDITOR nvim
 set --export PAGER less
@@ -94,8 +94,62 @@ function ssh-to
     end
 end
 
+function git-add-file
+    set file (git status --short | grep -E '^.[D|M|R|T|\?]' | fzf | awk '{ print $2 }' | xargs echo)
+
+    if test $status -eq 0
+        git add $file
+    else
+        return 1
+    end
+end
+
+function git-restore-file
+    set file (git status --short | grep -E '^[A|D|M|R|T]' | fzf | awk '{ print $2 }' | xargs echo)
+
+    if test $status -eq 0
+        git restore --staged $file
+    else
+        return 1
+    end
+end
+
 ########################################
 # Prompt
 ########################################
 starship init fish | source
 fish_vi_key_bindings
+
+# git add
+bind --mode default \cg\ca 'git-add-file; commandline -f repaint'
+bind --mode default ga 'git-add-file; commandline -f repaint'
+bind --mode insert \cg\ca 'git-add-file; commandline -f repaint'
+
+# git commit
+bind --mode default \cg\cc 'git commit; commandline -f repaint'
+bind --mode default ga 'git commit; commandline -f repaint'
+bind --mode insert \cg\cc 'git commit; commandline -f repaint'
+
+# git diff
+bind --mode default \cg\cd 'git diff; commandline -f repaint'
+bind --mode default gd 'git diff; commandline -f repaint'
+bind --mode insert \cg\cd 'git diff; commandline -f repaint'
+
+# git log
+bind --mode default \cg\cl 'git log; commandline -f repaint'
+bind --mode default gl 'git log; commandline -f repaint'
+bind --mode insert \cg\cl 'git log; commandline -f repaint'
+
+# git restore
+bind --mode default \cg\cr 'git-restore-file; commandline -f repaint'
+bind --mode default gr 'git-restore-file; commandline -f repaint'
+bind --mode insert \cg\cr 'git-restore-file; commandline -f repaint'
+
+# git status
+bind --mode default \cg\cs 'git status; commandline -f repaint'
+bind --mode default gs 'git status; commandline -f repaint'
+bind --mode insert \cg\cs 'git status; commandline -f repaint'
+
+#bind --mode insert \cg\cw\ca 'git worktree add'
+bind --mode insert \cg\cw\cl 'git worktree list; commandline -f execute'
+#bind --mode insert \cg\cw\cr 'git worktree remove'
